25b6a8173dba7dea4f436e49db71679f9701b251
MaratOnufrienko <maronu882@gmail.com>
2024-11-22 10:08:53 +0300

    first commit

---
+++ pz/pz4z1.py
@@ -0,0 +1,25 @@
+import math
+
+#Запрашиваем у пользователя вод данных
+X = float(input("Введите значение X: "))
+N = input("Введите значение N (целое и больше 0): ")
+
+#Обрабатываем исключения для N
+while True:
+    try:
+        N = int(N)
+        if N > 0:
+            break
+        else:
+            raise ValueError
+    except ValueError:
+        print("Неверный ввод! Пожалуйста, введите целое число больше 0.")
+        N = input("Введите значение N: ")
+
+#Вычисляем приближенное значение sin(X) через ряд Тейлора
+sin_approx = 0
+for k in range(N + 1):
+    term = ((-1) - k * X * (2 * k + 1)) / math.factorial(2 * k + 1)
+    sin_approx += term
+
+print("Приближенное значение sin(X):", sin_approx)
---
+++ pz/pz4z2.py
@@ -0,0 +1,17 @@
+# Ввод данных
+N = input("Введите значение N (целое и больше 0): ")
+
+# Обработка исключений для N
+while type(N) != int or N <= 0:
+    try:
+        N = int(N)
+        if N <= 0:
+            raise ValueError
+    except ValueError:
+        print("Неверный ввод! Пожалуйста, введите целое число больше 0.")
+        N = input("Введите значение N: ")
+
+# Вычисление суммы 1^N + 2^(N-1) + ... + N^1
+sum_result = sum(i ** (N - i + 1) for i in range(1, N + 1))
+
+print("Сумма 1^N + 2^(N-1) + ... + N^1 равна:", sum_result)
